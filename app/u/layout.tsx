4613d35173276641c09556c3c21d9987dcf6ccbb
takuyayamada1982 <takuyayamada1982@gmail.com>
2025-11-03 01:02:20 +0900

    u layout.tsx

---
+++ app/u/layout.tsx
@@ -0,0 +1,7 @@
+// app/u/layout.tsx
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
