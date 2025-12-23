091e154e5f986674067bcac5203c6d86ab956c76
takuyayamada1982 <takuyayamada1982@gmail.com>
2025-12-23 13:25:06 +0900

    Update page.tsx

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import AuthClient from './AuthClient';
 
 export default function AuthPage() {
-  return <AuthClient />;
+  return (
+    <Suspense fallback={<div />}>
+      <AuthClient />
+    </Suspense>
+  );
 }
